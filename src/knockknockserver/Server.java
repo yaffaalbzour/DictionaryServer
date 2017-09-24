7222ab24afbee1ae58494760ca8ce65a343fc64f
yaffaalbzour <yaffaalbzour@users.noreply.github.com>
2017-09-24 18:46:45 +0300

    Initial commit

---
+++ src/knockknockserver/Server.java
@@ -0,0 +1,36 @@
+package knockknockserver;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException 
+    {
+        try ( 
+            ServerSocket serverSocket = new ServerSocket(1234);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+        
+            String word, meaning;
+            
+            // Initiate conversation with client
+            Dictionary result = new Dictionary();
+            meaning = result.processInput(null);
+            out.println(meaning);
+
+            while ((word = in.readLine()) != null) {
+                meaning = result.processInput(word);
+                out.println(meaning);
+                if (meaning.equals("Bye."))
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println("Error in connection attempt.");
+            System.out.println(e.getMessage());
+        }
+    }
+}
